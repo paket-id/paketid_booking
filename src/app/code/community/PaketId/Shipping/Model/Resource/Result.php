911411e4dc28919e2eb298f38774271b0e049399
maimunah1994 <may@paket.id>
2016-09-20 09:14:26 +0700

    delete result resource

--- src/app/code/community/PaketId/Shipping/Model/Resource/Result.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class PaketId_Shipping_Model_Resource_Result extends Mage_Core_Model_Resource_Db_Abstract {
-	public function _construct()
-    {
-        $this->_init('paketid_shipping/result', 'entity_id');
-    }
-	
-}
